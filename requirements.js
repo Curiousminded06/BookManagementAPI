9e83cf535cb7b78de865874f0cabb98ed08af48e
Sahil <sahil2456.rsh@gmail.com>
2024-07-16 19:03:11 +0530

    all get methods included

---
+++ requirements.js
@@ -0,0 +1,35 @@
+//requirements for our project
+// We are a book management company
+
+//data that our company has -so deal with insights
+//books
+//ISBN,title,pub date ,language,num page,author,category
+
+//authors
+//id,name,books[]
+
+//publications
+//id,name,books[]
+
+
+//what are all the API's that we need
+
+//Books
+//we need an api 
+//to get all books✅
+// to get specific books✅
+// to get list of books based on category✅
+//to get list of books based on lanaguages✅
+
+
+//authors
+//we need an Api
+// to get all authors ✅
+// to get specific authors✅
+// to get list of authors based on books✅
+
+// Publications
+// we need an api
+// to get all publications ✅
+//to get specific publication✅
+// to get list of publications based on books✅
